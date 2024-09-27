d217f283b27cd3d458b846429fe21cf4d3bb262f
harsgsirt <tbtharshgupta@gmail.com>
2024-09-27 15:05:36 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,24 +1,19 @@
-def isValid(s: str) -> bool:
- 
-    stack = []
-    
-    bracket_map = {')': '(', '}': '{', ']': '['}
-    
-  
-    for char in s:
-        if char in bracket_map:
-           
-            top_element = stack.pop() if stack else '#'
-            
-            
-            if bracket_map[char] != top_element:
-                return False
-        else:
-           
-         stack.append(char)
-    
-   
-    return not stack
+# program1.py
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+
 
 
 
