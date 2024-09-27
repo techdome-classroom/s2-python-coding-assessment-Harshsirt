02198dc612206de0132411f26233c25f0408bb87
harsgsirt <tbtharshgupta@gmail.com>
2024-09-27 14:08:10 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,10 +1,24 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+def isValid(s: str) -> bool:
+    # Stack to keep track of opening brackets
+    stack = []
+    # Mapping of closing to opening brackets
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    
+    # Traverse the string
+    for char in s:
+        if char in bracket_map:
+            # Pop from stack if stack is not empty, otherwise assign dummy value '#'
+            top_element = stack.pop() if stack else '#'
+            
+            # If the mapping for the closing bracket doesn't match the top of stack
+            if bracket_map[char] != top_element:
+                return False
+        else:
+            # Push the opening bracket onto the stack
+            stack.append(char)
+    
+    # If the stack is empty, return True, else False
+    return not stack
 
 
 
