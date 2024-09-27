991484f63c7fd193597de9aa337a1b39e87e9cae
harsgsirt <tbtharshgupta@gmail.com>
2024-09-27 14:08:38 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,7 +1,7 @@
 def isValid(s: str) -> bool:
     # Stack to keep track of opening brackets
     stack = []
-    # Mapping of closing to opening brackets
+    
     bracket_map = {')': '(', '}': '{', ']': '['}
     
   
