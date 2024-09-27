b491f64ef98a9130f334d89e1e7c8421f69b13fc
harsgsirt <tbtharshgupta@gmail.com>
2024-09-27 14:13:01 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,10 +1,28 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # Mapping of Roman numerals to integers
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If this is not the last character and the next character is greater than the current one
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]  # Subtract the current value
+            else:
+                total += roman_map[s[i]]  # Add the current value
+                
+        return total
+
 
 
 
